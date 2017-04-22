b02b501307d07aa61250106e6fdfebdcb23673a0
Artem Grodeckiy <gal20040@gmail.com>
2017-04-23 00:06:54 +0300

    переделал тестирование.

--- TCS_2017/ParsingRegularExpressions/testing.py
+++ TCS_2017/ParsingRegularExpressions/testing.py
@@ -1,36 +1,38 @@
 import unittest
-from TCS_2017.ParsingRegularExpressions.automaton import reg_exp_parse
+
+import Parser
+from __init__ import remove_redundant_breakspaces
+from InputReader import InputReader
 
 
 class TestRegex(unittest.TestCase):
     def base(self, file_name):
-        with open(file_name) as file:
-            self.text = file.readlines()
-        
-        for line in self.text:
-            llist = line.split()
-            reg_exp = None
-            true_string = None
-            false_string = None
-            if len(llist) == 2:
-                [reg_exp, true_string] = llist
-            elif len(llist) == 3:
-                [reg_exp, true_string, false_string] = llist
-            automaton = reg_exp_parse(reg_exp)
+        input_reader = InputReader(file_name)
 
-            result = automaton.validate(true_string)
-            if result is False:
-                self.assertEqual(False, True)
+        reg_exp = input_reader.read_one_line()
+        while reg_exp != "":
+            ndfsa = Parser.reg_exp_parse(reg_exp)
 
-            if false_string:
-                result = automaton.validate(false_string)
-                if result is not False:
-                    self.assertEqual(True, False)
+            test_cases_list = input_reader.read_test_cases_and_results_from_input_file()
 
-            print(line, "pass")
-    
+            for j in range(len(test_cases_list)):
+                result_string = ndfsa.validate(test_cases_list[j][0])
+
+                if not result_string:  # for cases: result = False
+                    result_string = ""
+
+                result_string = remove_redundant_breakspaces(result_string)
+                test_case_result = test_cases_list[j][1]
+                self.assertEqual(result_string, test_case_result)
+
+                print(reg_exp + " " + test_case_result, "pass")
+
+            reg_exp = input_reader.read_one_line()
+
+        input_reader.close_input_file()
+
     def test_basic(self):
-        self.base('test_suite.dat')
+        self.base('tests.txt')
 
 if __name__ == '__main__':
     unittest.main()
