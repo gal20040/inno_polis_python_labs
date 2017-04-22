26dd536fba69987f36d5d73c7830e34df6161665
Artem Grodeckiy <gal20040@gmail.com>
2017-04-22 20:21:31 +0300

    убрал ненужные методы.

--- TCS_2017/ParsingRegularExpressions/InputReader.py
+++ TCS_2017/ParsingRegularExpressions/InputReader.py
@@ -3,25 +3,8 @@ class InputReader(object):
         input_file_name = "input.txt"
         self.input_file = open(input_file_name, 'r')  # 'r' - open for reading
 
-    # """read new line from input file and replace unnecessary signs - \n and { and }"""
-    # def __readline_and_cleanout(self):
-    #     string = self.input_file.readline().replace('\n', '')
-    #     string = string.replace('{', '').replace('}', '')
-    #     return string
-
     def read_one_line(self):
         return self.input_file.readline().replace('\n', '')
-
-    # # def read_description_from_input_file(self):
-    # #     automaton_description_lines = 5
-    # #     description_list = []
-    # #
-    # #     """read the automaton description"""
-    # #     for i in range(automaton_description_lines):
-    # #         string_description = self.__readline_and_cleanout()
-    # #         description_list.append(string_description)
-    # #
-    # #     return description_list
 
     def read_test_cases_from_input_file(self):
         test_case_number = int(self.read_one_line())
